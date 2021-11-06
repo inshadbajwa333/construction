9e860d82a6e4a6f8aa0fe10ae7344d4801368243
inshadbajwa333 <inshadbajwa333@gmail.com>
2021-11-06 18:22:30 +0400

    construction dynamics

--- application/views/theme/construction.php
+++ application/views/theme/construction.php
@@ -1,162 +1,105 @@
 <style>
     
-    .slide {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 400px;
-        margin: 0;
-        opacity: 0;
-        transition: 1s ease-in-out;
-        overflow: hidden;
-    }
+   
+/* slideshow taşıyıcı */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
 
-    .slide img {
-        width: 100%;
-    }
+/* varsayılan olarak görselleri gizliyoruz */
+.mySlides {
+  display: none;
+}
 
-    button {
-        position: absolute;
-        top: 50%;
-        border: none;
-        background: rgba(32, 26, 26, 0.527);
-        color: rgb(243, 243, 243);
-        padding: 10px 16px;
-        margin-top: -25px;
-        font-size: 30px;
-        z-index: 1000;
-        font-weight: 900;
-        transition: 0.5s ease-in-out;
-    }
+/* sonraki ve önceki butonları */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
 
-    .prev {
-        left: 0;
-    }
+/* sonraki butonunun pozisyonu */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
 
-    .next {
-        right: 0;
-    }
+/* üzerine gelince hafif bir efekt verelim */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
 
+/* görsel başlığı */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
 
-    button:hover {
-        background: rgba(32, 26, 26, 0.527);
-    }
+/* görsel numaraları */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
 
-    .dots_container {
-        display: flex;
-        margin: 5px auto;
-        width: fit-content;
-    }
+/* nokta navigasyonu */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
 
-    .dots {
-        height: 12px;
-        width: 12px;
-        border-radius: 50%;
-        background: #bdbdbd;
-        margin: 4px;
-    }
+.active, .dot:hover {
+  background-color: #717171;
+}
 
-    .dots:hover {
-        background: #696969 !important;
-    }
+/* geçiş animasyonu */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
 
-    @media screen and (max-width:600px) {
-      .slide{
-        height:fit-content;
-      }
-      .container{
-height:200px;}
-    button {
-        top: 50%;
-    }
-    }
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
 
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+.fade {
+    opacity: 1 !important;
+    transition: opacity .15s linear;
+}
     </style>
 
-    <script>
-         var slide = document.getElementsByClassName("slide");
-        var indicator = document.getElementById("indicator");
-        var dots = document.getElementsByClassName("dots");
-        var autoplay = document.getElementsByClassName("container")[0].getAttribute("data-autoplay");
-        var l = slide.length;
-        var interval = 5000;
-        var set;
-
-        window.onload = function () {
-            initialize();
-            slide[0].style.opacity = "1";
-            for (var j = 0; j < l; j++) {
-                indicator.innerHTML += "<div class='dots' onclick=change(" + j + ")></div>";
-            }
-
-            dots[0].style.background = "#696969";
-
-        }
-
-        function initialize() {
-            if (autoplay === "true")
-                set = setInterval(function () { next(); }, interval);
-        }
-
-
-
-        function change(index) {
-            clearInterval(set);
-            count = index;
-            for (var j = 0; j < l; j++) {
-                slide[j].style.opacity = "0";
-                dots[j].style.background = "#bdbdbd";
-            }
-            slide[count].style.opacity = "1";
-            dots[count].style.background = "#696969";
-
-
-        }
-
-        var count = 0;
-        function next() {
-            clearInterval(set);
-            slide[count].style.opacity = "0";
-            count++;
-            for (var j = 0; j < l; j++) {
-                dots[j].style.background = "#bdbdbd";
-            }
-
-
-            if (count == l) {
-                count = 0;
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-
-            } else {
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-            }
-            initialize()
-        }
-
-
-        function prev() {
-            clearInterval(set);
-            slide[count].style.opacity = "0";
-            for (var j = 0; j < l; j++) {
-                dots[j].style.background = "#bdbdbd";
-            }
-            count--;
-
-            if (count == -1) {
-                count = l - 1;
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-
-            } else {
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-            }
-            initialize()
-        }
-        </script>
+ 
 <div class="containe">
         
         <div class="block1">
@@ -170,49 +113,54 @@ height:200px;}
                 <div class="col-md-6">
                     <h1 class="text-center">Before</h1>
                     <div class="col-sm box-styling">
-                      
+                        
                           
-    <div class="container" data-autoplay="true">
-    <?php foreach(unserialize($data1['w_before']) as $data2):  ?>
-        <div class="slide">
-             <img src="<?=base_url()?>assets/works/<?php print_r($data2); ?>" alt="nature" />
-            </div>
-            <?php endforeach; ?>
+  
 
-        <button class="prev" onclick="prev()"><i class="fa fa-angle-left"></i></button>
-        <button class="next" onclick="next()"><i class="fa fa-angle-right"></i></button>
+<div class="slideshow-container" >
+<?php
+$i = 1;
+foreach(unserialize($data1['w_before']) as $data2):  ?>
+<div class="mySlides fade" style="margin:auto;text-align:center;">
+  <div class="numbertext">1 / 3</div>
+  <img src="<?=base_url()?>assets/works/<?php print_r($data2); ?>" style="width:40%">
+</div>
 
 
-    </div>
-    <div class="dots_container" id="indicator"></div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nemo ducimus, ipsa
-                        excepturi quis corporis quos nulla, consequatur error illum ab id repellendus perferendis et sed
-                        fugit deserunt quisquam obcaecati! Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                        placeat quaerat aliquid modi suscipit voluptatum a maiores dolor voluptatibus fugit doloribus, rem
-                        odio molestias itaque eius libero error sint mollitia earum. Neque pariatur natus, autem nostrum
-                        dolor, distinctio, commodi sapiente eligendi ipsum ut cumque corporis vero minima quis aliquam
-                        labore molestias sed doloremque fugit praesentium veritatis nihil! Eum tenetur ipsam porro
-                        consequuntur iste veritatis ex dolorum minus recusandae, qui nemo maxime beatae reiciendis! Aliquid
-                        doloremque dicta sequi cum aut esse excepturi. Maxime ullam impedit officia nobis ipsum perferendis
-                        maiores quod, porro similique numquam delectus est ratione accusantium nisi labore temporibus.
+<?php endforeach; ?>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+
+
+                        <?=$data1['w_before_explain']?>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <h1 class="text-center">After</h1>
                     <div class="col-sm box-styling">
-                    <?php foreach(unserialize($data1['w_after']) as $data2):  ?>
-                          <img src="<?=base_url()?>assets/works/<?php print_r($data2); ?>" style="margin-left: 10px;margin-right: 10px;display: block;width:30%;">
-                          <?php endforeach; ?>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nemo ducimus, ipsa
-                        excepturi quis corporis quos nulla, consequatur error illum ab id repellendus perferendis et sed
-                        fugit deserunt quisquam obcaecati! Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                        placeat quaerat aliquid modi suscipit voluptatum a maiores dolor voluptatibus fugit doloribus, rem
-                        odio molestias itaque eius libero error sint mollitia earum. Neque pariatur natus, autem nostrum
-                        dolor, distinctio, commodi sapiente eligendi ipsum ut cumque corporis vero minima quis aliquam
-                        labore molestias sed doloremque fugit praesentium veritatis nihil! Eum tenetur ipsam porro
-                        consequuntur iste veritatis ex dolorum minus recusandae, qui nemo maxime beatae reiciendis! Aliquid
-                        doloremque dicta sequi cum aut esse excepturi. Maxime ullam impedit officia nobis ipsum perferendis
-                        maiores quod, porro similique numquam delectus est ratione accusantium nisi labore temporibus.
+                    <div class="slideshow-container" >
+<?php
+$i = 1;
+foreach(unserialize($data1['w_after']) as $data2):  ?>
+<div class="mySlides fade" style="margin:auto;text-align:center;">
+  <div class="numbertext">1 / 3</div>
+  <img src="<?=base_url()?>assets/works/<?php print_r($data2); ?>" style="width:40%">
+</div>
+
+
+<?php endforeach; ?>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+<?=$data1['w_after_explain']?>
                     </div>
                 </div>
             </div>
@@ -225,3 +173,33 @@ height:200px;}
         </div>
         
     </div>
+
+
+    <script>
+       var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+        </script>
